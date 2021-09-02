d3c3716f951623ad42e7ee8842fb9dcd04b30dfe
rickstaa <rick.staa@outlook.com>
2021-09-02 15:52:27 +0200

    Adds effort and position control examples

--- real_panda_control_examples/README.md
+++ real_panda_control_examples/README.md
@@ -1,4 +1,8 @@
 # Real panda MoveIt control
 
 This folder contains several launch scripts that can be used to control the real panda_robot using
-MoveIt.
+[MoveIt](https://moveit.ros.org/) and [ros_control](https://wiki.ros.org/ros_control).
+
+## Warning
+
+Please be very careful when using this script on the real robot as the velocity limits are very high!
---
+++ tests/manual/joint_control_virtual_disturbance_test.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+"""Small script to test if we can add a artificial noise to the panda joint commands.
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!IMPORTANT!!!!!!!!!!!!!!!!!!!!!!!!!!
+PLEASE BE CARFULL WHEN USING THIS SCRIPT WITH THE REAL ROBOT.
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+"""
+
+import rospy
+from std_msgs.msg import Float64
+import time
+import math
+import numpy as np
+from sensor_msgs.msg import JointState
+
+# Control settings
+CONTROL_TYPE = "effort"  # NOTE: Options are "effort" and "position"
+ACTION_PUB_FREQ = 1000
+ACTION_MAGNITUDE = 2.0
+ACTION_FREQ = 0.5
+
+# Disturbance settings
+DIST_TYPE = None  # NOTE: Options are "impulse", "constant_impulse" and "random"
+DISTURBANCE_MAGNITUDE = 0.0
+DIST_FREQ = 0.5
+DIST_START_TIME = 2.0
+
+
+def get_disturbance(action):
+    """Retrieves a given disturbance based on a disturbance type.
+
+    Args:
+        action (np.ndarray): The action the disturbance should be applied to.
+
+    Returns:
+        np.ndarray: The disturbance.
+    """
+    t = time.time()
+    if DIST_TYPE == "impulse":
+        if math.isclose((t - START_TIME) % (1 / DIST_FREQ), 0.0, abs_tol=0.1):
+            return DISTURBANCE_MAGNITUDE * (-np.sign(action))
+        else:
+            return np.zeros_like(action)
+    if DIST_TYPE == "constant_impulse":
+        if t >= DIST_START_TIME:
+            return DISTURBANCE_MAGNITUDE * (-np.sign(action))
+    elif DIST_TYPE == "random":
+        return DISTURBANCE_MAGNITUDE * np.random.rand(*action.shape) * -np.sign(action)
+    else:
+        return np.zeros_like(action)
+
+
+if __name__ == "__main__":
+    rospy.init_node("joint_control_virtual_disturbance_test", anonymous=False)
+
+    # Create required publishers
+    publishers = [
+        rospy.Publisher(
+            f"/panda_arm_joint{idx}_{CONTROL_TYPE}_controller/command",
+            Float64,
+            queue_size=10,
+        )
+        for idx in range(1, 8)
+    ]
+
+    # Get actual initial joint states
+    init_joint_states = rospy.wait_for_message("/joint_states", JointState)
+    init_joint_states = dict(
+        zip(
+            init_joint_states.name,
+            init_joint_states.position
+            if CONTROL_TYPE == "position"
+            else init_joint_states.effort,
+        )
+    )
+
+    # Publish sinusoid joint command and disturbance
+    START_TIME = time.time()
+    while True:
+
+        # Create disturbed action
+        action = np.array(
+            [
+                ACTION_MAGNITUDE
+                * math.sin(2 * np.pi * ACTION_FREQ * (time.time() - START_TIME)),
+            ]
+        )
+        d = get_disturbance(action)
+        d_action = action + d
+
+        # Send control command + disturbance
+        publishers[0].publish(init_joint_states["panda_joint1"] + d_action[0])
+
+        # Force publishing rate
+        time.sleep(1 / ACTION_PUB_FREQ)
